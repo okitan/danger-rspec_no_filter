c239a8b2f8f5a747b085fddad7ab0c7d17f9a97d
okitan <okitakunio@gmail.com>
2017-07-31 20:48:41 +0900

    :bulb: make it as plugin

--- lib/rspec_no_filter/plugin.rb
+++ lib/rspec_no_filter/plugin.rb
@@ -1,3 +1,5 @@
+require "git_diff_parser"
+
 module Danger
   # This is your plugin class. Any attributes or methods you expose here will
   # be available from within your Dangerfile.
@@ -17,17 +19,30 @@ module Danger
   # @tags monday, weekends, time, rattata
   #
   class DangerRspecNoFilter < Plugin
-
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
-
     # A method that you can call from your Dangerfile
     # @return   [Array<String>]
     #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    def warn_for_rspec_filter_tags(tags = tags_from_spec_helper)
+      regexp = Regexp.new([ "fit", "fcontext", *tags ].map {|item| "\\s+#{item}\\s+" }.join("|"))
+
+      diff = GitDiffParser.parse(github.pr_diff)
+
+      diff.each do |diff_per_file|
+        next unless diff_per_file.file.end_with?(".rb") # for shared examples _spec.rb is not enough
+
+        diff_per_file.changed_lines.each do |line|
+          if line.content =~ regexp
+            warn "#{line.content} on #{diff_per_file.file}:#{line.number} includes #{regexp}"
+          end
+        end
+      end
+    end
+
+    protected
+    def tags_from_spec_helper
+      `bundle exec ruby -r rspec -r ./spec/spec_helper -e "print RSpec.configuration.filter_run.merge(RSpec.configuration.filter_run_when_matching).keys.join(',')"`.split(",").map {|item| "\\:#{item}" }
+    rescue
+      []
     end
   end
 end
